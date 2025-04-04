bc7fc6f6f1652747ce610a94d50fe4ee294e860c
BpReddy1104 <bpreddy0011@gmail.com>
2025-04-04 12:27:33 +0530

    Initial commit - AI-Powered CI/CD project

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
+import os
 from flask import Flask
-import sqlite3
 
 app = Flask(__name__)
 
-def get_db_connection():
-    conn = sqlite3.connect("database.db")
-    conn.row_factory = sqlite3.Row
-    return conn
-
 @app.route("/")
 def home():
-    return "Hello, DevOps with AI! Database Connected."
+    return "Hello, DevOps with AI!"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from Render, default to 5000
+    app.run(debug=True, host="0.0.0.0", port=port)  # Bind to 0.0.0.0
